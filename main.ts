a5b5697c8e0e58f58046add6e95977dc224435d2
TrafalgarMat <103508401+TrafalgarMat@users.noreply.github.com>
2022-04-12 14:30:12 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        basic.showString("T=")
+        basic.showNumber(input.temperature())
+        basic.showString("DEGRES")
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(500)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
